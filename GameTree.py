d120829d35b6117e6c62b7f0b660a1b79bd5f74b
Emilija Kalnača <144778599+Emilik0@users.noreply.github.com>
2025-03-19 11:57:55 +0200

    Update GameTree.py

--- GameTree.py
+++ GameTree.py
@@ -1,4 +1,3 @@
-
 class TreeNode:
     def __init__(self, data):
         self.data = data
@@ -8,34 +7,40 @@ class TreeNode:
         self.p2points = 0
         self.turnCount = 0
 
-    def addChild(self, data):
-        data.p1points = self.p1points
-        data.p2points = self.p2points
-        data.turnCount = self.turnCount + 1
-        self.children.append(data)
+    def addChild(self, node):
+        node.p1points = self.p1points
+        node.p2points = self.p2points
+        node.turnCount = self.turnCount + 1
+        self.children.append(node)
 
     # Atļauj mums salīdzināt mūsu TreeNodes objektus ar == operatoru, šobrīd vēl neizmantoju
     # ,bet kad vajadzēs salīdzināt nodes, lai neveidotu liekus aprēķinus gan jau noderēs
     def __eq__(self, other):
-        return self.p1points == other.p1points and self.p2points == other.p2points and self.turnCount == other.turnCount
+        return self.p1points == other.p1points and self.p2points == other.p2points and self.turnCount == other.turnCount and self.data == other.data
 
+    def __hash__(self):
+        return hash((self.data, self.p1points, self.p2points, self.turnCount))
 
 def buildTree(startNumb):
     root = TreeNode(startNumb)
-    createLevel(root)
+    createdNodes = set()
+    createLevel(root, createdNodes) 
     return root
 
-def createLevel(parentNode):
+def createLevel(parentNode, createdNodes):
     number = parentNode.data
     if number >= 1200:
         return
-    else:
-        nodes = [TreeNode(number * 2), TreeNode(number * 3), TreeNode(number * 4)]
+    
+    nodes = [TreeNode(number * 2), TreeNode(number * 3), TreeNode(number * 4)]
 
     for node in nodes:
-        parentNode.addChild(node)
-        addPoints(node)
-        createLevel(node)
+        
+        if node not in createdNodes:
+            parentNode.addChild(node)
+            addPoints(node)
+            createdNodes.add(node)
+            createLevel(node, createdNodes)
 
 def addPoints(node):
     if node.turnCount % 2 == 1:
@@ -51,11 +56,26 @@ def addPoints(node):
 
 def printTree(root):
     for node in root.children:
-        print("--"*node.turnCount + ">" + str(node.data))
+        print("--"*node.turnCount + ">" + str(node.data)+ f" (P1: {node.p1points}, P2: {node.p2points})")
         printTree(node)
+
+def printFinalNodes(root):
+    def traverse(node, visited):
+        if node in visited:
+            return
+        visited.add(node)
+        
+        if not node.children:
+            print("--" * node.turnCount + ">" + str(node.data) + f" (P1: {node.p1points}, P2: {node.p2points})")
+        
+        for child in node.children:
+            traverse(child, visited)
+    
+    traverse(root, set())
 
 if __name__ == '__main__':
     Tree = buildTree(8)
-    print(8)
     printTree(Tree)
-    print(Tree)
+    print("\nFinal Nodes:")
+    printFinalNodes(Tree)
+
